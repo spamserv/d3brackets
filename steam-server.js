e78a9b733370e759a59267512167bea3194a9471
Josip Vojak <josipvojak@gmail.com>
2016-05-29 19:22:22 +0200

    Added steam-web package, getting steam info

--- steam-server.js
+++ steam-server.js
@@ -1,15 +1,25 @@
 var express = require('express');
-var app = express();
 var http = require('http');
+var steam = require('steam-web');
+var app = express();
 var port = 8081;
 var server = http.createServer(app).listen(port);
+var steamClient = new steam({
+  apiKey: '988FF43E45F6E038286BEEAC22A3155B',
+  format: 'json' //optional ['json', 'xml', 'vdf']
+})
 
 io = require('socket.io').listen(server);
 
 io.on('connection', function (socket){
 	console.log('connected');
 	socket.on('steam info', function(steam_ids, callback){
-		callback("broj");
+		steamClient.getPlayerSummaries({
+		  steamids: steam_ids,
+		  callback: function(err, data) {
+		    console.log(data.response.players);
+		  }
+		});
 		
 	});
 });
